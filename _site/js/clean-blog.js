607534cc1198db3d6d7cacab898eeb7feca66787
Houssem Zaier <houssem.zaier@gmail.com>
2016-09-24 22:32:42 +0200

    fix email

--- _site/js/clean-blog.js
+++ _site/js/clean-blog.js
@@ -26,7 +26,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "//formspree.io/houssem.zaier@gmail.com",
                 type: "POST",
                 data: {
                     name: name,
